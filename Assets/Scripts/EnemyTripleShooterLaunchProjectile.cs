6d440eb9920c4e758f8ec71cff656d58908591b4
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-04-26 23:20:17 -0500

    created triple-shooter prefab
    no code yet exists to implement this prefab, but at least now I know the triple shooter works

---
+++ Assets/Scripts/EnemyTripleShooterLaunchProjectile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTripleShooterLaunchProjectile : MonoBehaviour
+{
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] float minRepeatTime = 0.5f;
+    [SerializeField] float maxRepeatTime = 1.5f;
+    private AudioSource firingNoise;
+    [SerializeField] GameObject bulletSpawnerLeft;
+    [SerializeField] GameObject bulletSpawnerRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        firingNoise = GetComponent<AudioSource>();
+        StartCoroutine("LaunchProjectile");
+    }
+
+    IEnumerator LaunchProjectile()
+    {
+        while (true)
+        {
+            float waitTime = Random.Range(minRepeatTime, maxRepeatTime);
+            yield return new WaitForSeconds(waitTime);
+            firingNoise.Play();
+            Instantiate(bulletPrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation);
+            Instantiate(bulletPrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z), bulletSpawnerLeft.transform.rotation);
+            Instantiate(bulletPrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z), bulletSpawnerRight.transform.rotation);
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyTurretAimTripleShooter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTurretAimTripleShooter : MonoBehaviour
+{
+    private GameObject playerObject;
+    [SerializeField] float lowerBound = 1;
+    private EnemyTripleShooterLaunchProjectile bulletSpawner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerObject = GameObject.FindGameObjectWithTag("PlayerTurret");
+        bulletSpawner = GetComponentInChildren<EnemyTripleShooterLaunchProjectile>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > lowerBound)
+        {
+            gameObject.transform.LookAt(playerObject.transform);
+        }
+        else
+        {
+            gameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
+            bulletSpawner.StopAllCoroutines();
+        }
+    }
+}
