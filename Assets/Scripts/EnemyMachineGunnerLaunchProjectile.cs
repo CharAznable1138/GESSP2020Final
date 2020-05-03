ede002456e08734f3173540062503e144b1c74db
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-05-03 16:11:27 -0500

    Modified machine gunner script (read description)
    Machine gunners now fire in short bursts instead of constantly firing (constant rapid fire made them far too difficult to destroy)

--- Assets/Scripts/EnemyMachineGunnerLaunchProjectile.cs
+++ Assets/Scripts/EnemyMachineGunnerLaunchProjectile.cs
@@ -8,7 +8,8 @@ public class EnemyMachineGunnerLaunchProjectile : MonoBehaviour
     [SerializeField] float minRepeatTime = 0.5f;
     [SerializeField] float maxRepeatTime = 1.5f;
     private AudioSource firingNoise;
-    [SerializeField] float startDelay = 1.0f;
+    [SerializeField] float burstDelay = 1.0f;
+    [SerializeField] int maxBulletsPerBurst = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -19,13 +20,18 @@ public class EnemyMachineGunnerLaunchProjectile : MonoBehaviour
 
     IEnumerator LaunchProjectile()
     {
-        yield return new WaitForSeconds(startDelay);
         while (true)
         {
-            float waitTime = Random.Range(minRepeatTime, maxRepeatTime);
-            yield return new WaitForSeconds(waitTime);
-            firingNoise.Play();
-            Instantiate(bulletPrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation);
+            yield return new WaitForSeconds(burstDelay);
+            int bulletsLaunched = 0;
+            while (bulletsLaunched < maxBulletsPerBurst)
+            {
+                float waitTime = Random.Range(minRepeatTime, maxRepeatTime);
+                yield return new WaitForSeconds(waitTime);
+                firingNoise.Play();
+                Instantiate(bulletPrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation);
+                bulletsLaunched++;
+            }
         }
     }
 }
