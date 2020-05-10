630e10415ea4d76ce467be2f96f55659e5179617
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-05-10 14:35:20 -0500

    made "Retry" and "NextLevel" buttons reusable across multiple scenes using GetActiveScene

--- Assets/Scripts/MainButtonManager.cs
+++ Assets/Scripts/MainButtonManager.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+//Credit to this thread for info on how to check the current scene index: https://answers.unity.com/questions/1114722/getting-scene-index-as-int.html
 
 public class MainButtonManager : MonoBehaviour
 {
+    private int currentSceneIndex;
+    private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
     public void QuitGame() => SceneManager.LoadScene(0);
 
-    public void Retry() => SceneManager.LoadScene(1);
+    public void Retry() => SceneManager.LoadScene(currentSceneIndex);
 
-    public void NextLevel() => SceneManager.LoadScene(2);
+    public void NextLevel() => SceneManager.LoadScene(currentSceneIndex + 1);
 }
