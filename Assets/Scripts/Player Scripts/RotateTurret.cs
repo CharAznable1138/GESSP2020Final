f9170202aa0c4798e0e0091b085d221ca9c1da3b
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-05-15 14:51:56 -0500

    deleted the old turret rotation logic
    I'd originally kept it commented out in case I ever wanted to give the player the option to choose between the two, but I've thought it over and concluded nobody would ever choose the old system because the new system is objectively superior.

--- Assets/Scripts/Player Scripts/RotateTurret.cs
+++ Assets/Scripts/Player Scripts/RotateTurret.cs
@@ -17,32 +17,6 @@ public class RotateTurret : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Two turn logics, old and new. Keeping the old in case I want to give players the option to toggle between the two later.
-
-        //Old turn logic
-
-        /*if (transform.rotation.y < -turnLimit)
-        {
-            transform.rotation = new Quaternion(transform.rotation.x, -turnLimit, transform.rotation.z, transform.rotation.w);
-        }
-
-        if (transform.rotation.y > turnLimit)
-        {
-            transform.rotation = new Quaternion(transform.rotation.x, turnLimit, transform.rotation.z, transform.rotation.w);
-        }
-        if (Input.GetKey(KeyCode.Q))
-        {
-            transform.Rotate(0, -turnSpeed * Time.deltaTime, 0);
-            
-        }
-        if(Input.GetKey(KeyCode.E))
-        {
-            transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
-        }*/
-
-        //New turn logic
-
         transform.LookAt(cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.transform.position.y - transform.position.y)));
-
     }
 }
