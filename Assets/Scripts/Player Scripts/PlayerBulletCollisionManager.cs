cfede8dc0971c2cb4866425adf98ea97011e4576
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-05-17 21:51:00 -0500

    implemented noise for bullet-on-bullet collision

--- Assets/Scripts/Player Scripts/PlayerBulletCollisionManager.cs
+++ Assets/Scripts/Player Scripts/PlayerBulletCollisionManager.cs
@@ -2,11 +2,13 @@
 
 public class PlayerBulletCollisionManager : MonoBehaviour
 {
+    [SerializeField] GameObject bulletCollisionNoise;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("EnemyProjectile"))
         {
+            Instantiate(bulletCollisionNoise, gameObject.transform.position, gameObject.transform.rotation);
             Destroy(gameObject);
             Destroy(collision.gameObject);
         }
---
+++ Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour
+{
+    [SerializeField] float sdDelay = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DestroySelf", sdDelay);
+    }
+
+    public void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+}
